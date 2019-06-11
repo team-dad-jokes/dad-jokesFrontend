ad23e15c693be7c891c876f275a224e8978f481d
kingtamaaj <kingtamaaj@gmail.com>
2019-06-11 19:17:14 -0400

    Add Toolbar

--- dad-jokes-app/src/App.js
+++ dad-jokes-app/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Toolbar from './components/Toolbar/Toolbar';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Toolbar />
+       </div>
+    )
+  }
 }
 
-export default App;
+export default App; 
---
+++ dad-jokes-app/src/components/Toolbar/Toolbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Toolbar.css';
+
+const toolbar = props => (
+    <header>
+        <nav>
+            <div> </div>
+            <div><a href="/">THE LOGO</a> </div>
+            <div> 
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Search</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+)
+
+export default toolbar; 
