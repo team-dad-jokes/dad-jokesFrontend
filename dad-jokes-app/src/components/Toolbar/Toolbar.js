326a31a6b88dae78f4040bb9f8d1815f02a89092
kingtamaaj <kingtamaaj@gmail.com>
2019-06-12 20:28:40 -0400

    Finished Navbar and pullout Sidebar

--- dad-jokes-app/src/components/Toolbar/Toolbar.js
+++ dad-jokes-app/src/components/Toolbar/Toolbar.js
@@ -4,13 +4,16 @@ import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 
 
 
+
 const Toolbar = props => (
     <header className="toolbar">
         <nav className="toolbar_navigation">
-            <div>
-                <DrawerToggleButton />
+       
+            <div className="toolbar_toggle-button">
+                <DrawerToggleButton click={props.drawerClickHandler} />
+
               </div>
-            <div className="toolbar_logo"><a href="/">Dad Jokes</a> </div>
+            <div className="toolbar_logo"><a href="/">DAD JOKES</a> </div>
             <div className="spacer" />
             <div className="toolbar_navigation-items"> 
                 <ul>
